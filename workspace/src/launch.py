e1369a76ca79dfd4e8961026387bedf03f657ce4
TSoli <tariqsoliman2000@gmail.com>
2024-03-06 11:51:47 +1000

    Removed SLAM files and updated launch file

--- workspace/src/launch.py
+++ workspace/src/launch.py
@@ -1,69 +1,62 @@
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     nodes = [
         # Tello driver node
         Node(
-            package='tello',
-            executable='tello',
-            output='screen',
-            namespace='/',
-            name='tello',
+            package="tello",
+            executable="tello",
+            output="screen",
+            namespace="/",
+            name="tello",
             parameters=[
-                {'connect_timeout': 10.0},
-                {'tello_ip': '192.168.10.1'},
-                {'tf_base': 'map'},
-                {'tf_drone': 'drone'}
+                {"connect_timeout": 10.0},
+                {"tello_ip": "192.168.10.1"},
+                {"tf_base": "map"},
+                {"tf_drone": "drone"},
             ],
-            remappings=[
-                ('/image_raw', '/camera')
-            ],
-            respawn=True
+            remappings=[("/image_raw", "/camera")],
+            respawn=True,
         ),
-
         # Tello control node
         Node(
-            package='tello_control',
-            executable='tello_control',
-            namespace='/',
-            name='control',
-            output='screen',
-            respawn=False
+            package="tello_control",
+            executable="tello_control",
+            namespace="/",
+            name="control",
+            output="screen",
+            respawn=False,
         ),
-
         # RQT topic debug tool
         Node(
-            package='rqt_gui',
-            executable='rqt_gui',
-            output='screen',
-            namespace='/',
-            name='rqt',
-            respawn=False
+            package="rqt_gui",
+            executable="rqt_gui",
+            output="screen",
+            namespace="/",
+            name="rqt",
+            respawn=False,
         ),
-
         # RViz data visualization tool
         Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            namespace='/',
-            name='rviz2',
+            package="rviz2",
+            executable="rviz2",
+            output="screen",
+            namespace="/",
+            name="rviz2",
             respawn=True,
-            arguments=['-d', '/home/tentone/Git/tello-slam/workspace/src/rviz.rviz']
+            arguments=["-d", "/home/tentone/Git/tello-slam/workspace/src/rviz.rviz"],
         ),
-
         # Static TF publisher
         Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            namespace='/',
-            name='tf',
-            arguments=['0', '0', '0', '0', '0', '0', '1', 'map', 'drone'],
-            respawn=True
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            namespace="/",
+            name="tf",
+            arguments=["0", "0", "0", "0", "0", "0", "1", "map", "drone"],
+            respawn=True,
         ),
-
         # ORB SLAM
         # Node(
         #     package='orbslam2',
@@ -77,7 +70,6 @@ def generate_launch_description():
         #     ],
         #     arguments=['~/Git/tello-slam/libs/ORB_SLAM2/Vocabulary/ORBvoc.txt', '~/Git/tello-slam/workspace/src/orbslam2/config.yaml']
         # ),
-
         # Camera calibration node
         # Node(
         #     package='camera_calibration',
@@ -93,6 +85,5 @@ def generate_launch_description():
         #     ]
         # )
     ]
-
 
     return LaunchDescription(nodes)
